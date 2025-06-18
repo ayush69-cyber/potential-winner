be019211f2f0d4cf9d9efab2904150710cec1dec
ayush69-cyber <ayushkhanal161@gmail.com>
2025-06-18 07:18:14 +0545

    Refined a little

--- matrix.cpp
+++ matrix.cpp
@@ -4,8 +4,7 @@
 matrix::matrix() {
   rows = 2;
   columns = 2;
-
-  data = nullptr;
+  *data = NULL;
 }
 
 matrix::~matrix() { delete data; }
@@ -13,23 +12,33 @@ matrix::~matrix() { delete data; }
 matrix::matrix(int rows, int columns) {
   this->rows = rows;
   this->columns = columns;
+  data = new float*[rows];
+  for(int i = 0; i<= rows ; i++)
+  {
+    data[i] = new float[columns];
+  }
 
-  data = nullptr;
 }
 
 void matrix::set_data(float d[]) {
-  data = new float[rows * columns];
-
-  for (int i = 0; i < rows * columns; i++) {
-    this->data[i] = d[i];
+  int k = 0;
+  for(int i = 0; i<rows ; i++)
+  {
+    for(int j = 0; j<columns ; j++)
+    {
+      data[i][j] = d[k];
+      k++;
+    }
   }
 }
 
 void matrix::print() {
-  for (int i = 0; i < rows * columns; i++) {
-    std::cout << data[i] << '\t';
-    if ((i + 1) % columns == 0) {
-      std::cout << '\n';
+  for(int i = 0; i< rows ; i++)
+  {
+    for(int j = 0 ; j< columns ; j++)
+    {
+      std::cout << data[i][j] << '\t';
     }
+    std:: cout << std::endl;
   }
 }
